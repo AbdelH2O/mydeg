bb566c03a924543d290b5baf0662443bec017f4c
Hana <a.hana@aui.ma>
2022-12-29 18:56:47 +0100

    ommiting the main page for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,15 @@ import Image from 'next/image'
 import '../styles/Home.module.css'
 // import Degree from './degree/[id]'
 
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: false,
+    },
+  }
+}
+
 export default function Home() {
   return (
     <div className="w-screen h-screen overflow-hidden">
