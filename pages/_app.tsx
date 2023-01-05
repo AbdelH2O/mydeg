268a4268dbc41811e075efcba14276b4b3630eb8
Hana <a.hana@aui.ma>
2023-01-05 01:37:51 +0100

    Added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import Providers from '../components/Providers';
 import Loader from '../components/Loader';
 import { ToastContainer } from 'react-toastify';
+import { Analytics } from '@vercel/analytics/react';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Loader />
       <ToastContainer />
       <Component {...pageProps} />
+      <Analytics />
     </Providers>
   );
 }
